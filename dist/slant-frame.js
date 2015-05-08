5e76b90ea5209e75f4281af9e48bcb1f48af3e0e
Joseph Werle <joseph.werle@gmail.com>
2015-05-08 11:37:42 -0400

    misc cleanup

--- dist/slant-frame.js
+++ dist/slant-frame.js
@@ -97,7 +97,6 @@ var three = require('three.js')
   , fullscreen = require('fullscreen')
   , tpl = require('./src/template.html')
   , keycode = require('keycode')
-  , offset = require('offset')
   , path = require('path')
 
 /**
@@ -200,7 +199,7 @@ function Frame (parent, opts) {
 
   function set (p) {
     if (opts[p]) {
-      self.video[p] = opts[p];
+      self.video.setAttribute(p, opts[p]);
     }
   }
 
@@ -1446,7 +1445,7 @@ Frame.prototype.destroy = function () {
   return this;
 };
 
-}, {"three.js":2,"domify":3,"emitter":4,"events":5,"raf":6,"has-webgl":7,"fullscreen":8,"./src/template.html":9,"keycode":10,"offset":11,"path":12,"three-canvas-renderer":13}],
+}, {"three.js":2,"domify":3,"emitter":4,"events":5,"raf":6,"has-webgl":7,"fullscreen":8,"./src/template.html":9,"keycode":10,"path":11,"three-canvas-renderer":12}],
 2: [function(require, module, exports) {
 // File:src/Three.js
 
@@ -36448,8 +36447,8 @@ function parse(event) {
   }
 }
 
-}, {"event":14,"delegate":15}],
-14: [function(require, module, exports) {
+}, {"event":13,"delegate":14}],
+13: [function(require, module, exports) {
 var bind = window.addEventListener ? 'addEventListener' : 'attachEvent',
     unbind = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
     prefix = bind !== 'addEventListener' ? 'on' : '';
@@ -36486,7 +36485,7 @@ exports.unbind = function(el, type, fn, capture){
   return fn;
 };
 }, {}],
-15: [function(require, module, exports) {
+14: [function(require, module, exports) {
 /**
  * Module dependencies.
  */
@@ -36530,8 +36529,8 @@ exports.unbind = function(el, type, fn, capture){
   event.unbind(el, type, fn, capture);
 };
 
-}, {"closest":16,"event":14}],
-16: [function(require, module, exports) {
+}, {"closest":15,"event":13}],
+15: [function(require, module, exports) {
 var matches = require('matches-selector')
 
 module.exports = function (element, selector, checkYoSelf, root) {
@@ -36552,8 +36551,8 @@ module.exports = function (element, selector, checkYoSelf, root) {
   }
 }
 
-}, {"matches-selector":17}],
-17: [function(require, module, exports) {
+}, {"matches-selector":16}],
+16: [function(require, module, exports) {
 /**
  * Module dependencies.
  */
@@ -36601,8 +36600,8 @@ function match(el, selector) {
   return false;
 }
 
-}, {"query":18}],
-18: [function(require, module, exports) {
+}, {"query":17}],
+17: [function(require, module, exports) {
 function one(selector, el) {
   return el.querySelector(selector);
 }
@@ -36829,476 +36828,6 @@ module.exports = function(name){
 
 }, {}],
 11: [function(require, module, exports) {
-var support = require('dom-support')
-var getDocument = require('get-document')
-var withinElement = require('within-element')
-
-/**
- * Get offset of a DOM Element or Range within the document.
- *
- * @param {DOMElement|Range} el - the DOM element or Range instance to measure
- * @return {Object} An object with `top` and `left` Number values
- * @public
- */
-
-module.exports = function offset(el) {
-  var doc = getDocument(el)
-  if (!doc) return
-
-  // Make sure it's not a disconnected DOM node
-  if (!withinElement(el, doc)) return
-
-  var body = doc.body
-  if (body === el) {
-    return bodyOffset(el)
-  }
-
-  var box = { top: 0, left: 0 }
-  if ( typeof el.getBoundingClientRect !== "undefined" ) {
-    // If we don't have gBCR, just use 0,0 rather than error
-    // BlackBerry 5, iOS 3 (original iPhone)
-    box = el.getBoundingClientRect()
-
-    if (el.collapsed && box.left === 0 && box.top === 0) {
-      // collapsed Range instances sometimes report 0, 0
-      // see: http://stackoverflow.com/a/6847328/376773
-      var span = doc.createElement("span");
-
-      // Ensure span has dimensions and position by
-      // adding a zero-width space character
-      span.appendChild(doc.createTextNode("\u200b"));
-      el.insertNode(span);
-      box = span.getBoundingClientRect();
-
-      // Remove temp SPAN and glue any broken text nodes back together
-      var spanParent = span.parentNode;
-      spanParent.removeChild(span);
-      spanParent.normalize();
-    }
-  }
-
-  var docEl = doc.documentElement
-  var clientTop  = docEl.clientTop  || body.clientTop  || 0
-  var clientLeft = docEl.clientLeft || body.clientLeft || 0
-  var scrollTop  = window.pageYOffset || docEl.scrollTop
-  var scrollLeft = window.pageXOffset || docEl.scrollLeft
-
-  return {
-    top: box.top  + scrollTop  - clientTop,
-    left: box.left + scrollLeft - clientLeft
-  }
-}
-
-function bodyOffset(body) {
-  var top = body.offsetTop
-  var left = body.offsetLeft
-
-  if (support.doesNotIncludeMarginInBodyOffset) {
-    top  += parseFloat(body.style.marginTop || 0)
-    left += parseFloat(body.style.marginLeft || 0)
-  }
-
-  return {
-    top: top,
-    left: left
-  }
-}
-
-}, {"dom-support":19,"get-document":20,"within-element":21}],
-19: [function(require, module, exports) {
-var domready = require('domready')
-
-module.exports = (function() {
-
-	var support,
-		all,
-		a,
-		select,
-		opt,
-		input,
-		fragment,
-		eventName,
-		i,
-		isSupported,
-		clickFn,
-		div = document.createElement("div");
-
-	// Setup
-	div.setAttribute( "className", "t" );
-	div.innerHTML = "  <link/><table></table><a href='/a'>a</a><input type='checkbox'/>";
-
-	// Support tests won't run in some limited or non-browser environments
-	all = div.getElementsByTagName("*");
-	a = div.getElementsByTagName("a")[ 0 ];
-	if ( !all || !a || !all.length ) {
-		return {};
-	}
-
-	// First batch of tests
-	select = document.createElement("select");
-	opt = select.appendChild( document.createElement("option") );
-	input = div.getElementsByTagName("input")[ 0 ];
-
-	a.style.cssText = "top:1px;float:left;opacity:.5";
-	support = {
-		// IE strips leading whitespace when .innerHTML is used
-		leadingWhitespace: ( div.firstChild.nodeType === 3 ),
-
-		// Make sure that tbody elements aren't automatically inserted
-		// IE will insert them into empty tables
-		tbody: !div.getElementsByTagName("tbody").length,
-
-		// Make sure that link elements get serialized correctly by innerHTML
-		// This requires a wrapper element in IE
-		htmlSerialize: !!div.getElementsByTagName("link").length,
-
-		// Get the style information from getAttribute
-		// (IE uses .cssText instead)
-		style: /top/.test( a.getAttribute("style") ),
-
-		// Make sure that URLs aren't manipulated
-		// (IE normalizes it by default)
-		hrefNormalized: ( a.getAttribute("href") === "/a" ),
-
-		// Make sure that element opacity exists
-		// (IE uses filter instead)
-		// Use a regex to work around a WebKit issue. See #5145
-		opacity: /^0.5/.test( a.style.opacity ),
-
-		// Verify style float existence
-		// (IE uses styleFloat instead of cssFloat)
-		cssFloat: !!a.style.cssFloat,
-
-		// Make sure that if no value is specified for a checkbox
-		// that it defaults to "on".
-		// (WebKit defaults to "" instead)
-		checkOn: ( input.value === "on" ),
-
-		// Make sure that a selected-by-default option has a working selected property.
-		// (WebKit defaults to false instead of true, IE too, if it's in an optgroup)
-		optSelected: opt.selected,
-
-		// Test setAttribute on camelCase class. If it works, we need attrFixes when doing get/setAttribute (ie6/7)
-		getSetAttribute: div.className !== "t",
-
-		// Tests for enctype support on a form (#6743)
-		enctype: !!document.createElement("form").enctype,
-
-		// Makes sure cloning an html5 element does not cause problems
-		// Where outerHTML is undefined, this still works
-		html5Clone: document.createElement("nav").cloneNode( true ).outerHTML !== "<:nav></:nav>",
-
-		// jQuery.support.boxModel DEPRECATED in 1.8 since we don't support Quirks Mode
-		boxModel: ( document.compatMode === "CSS1Compat" ),
-
-		// Will be defined later
-		submitBubbles: true,
-		changeBubbles: true,
-		focusinBubbles: false,
-		deleteExpando: true,
-		noCloneEvent: true,
-		inlineBlockNeedsLayout: false,
-		shrinkWrapBlocks: false,
-		reliableMarginRight: true,
-		boxSizingReliable: true,
-		pixelPosition: false
-	};
-
-	// Make sure checked status is properly cloned
-	input.checked = true;
-	support.noCloneChecked = input.cloneNode( true ).checked;
-
-	// Make sure that the options inside disabled selects aren't marked as disabled
-	// (WebKit marks them as disabled)
-	select.disabled = true;
-	support.optDisabled = !opt.disabled;
-
-	// Test to see if it's possible to delete an expando from an element
-	// Fails in Internet Explorer
-	try {
-		delete div.test;
-	} catch( e ) {
-		support.deleteExpando = false;
-	}
-
-	if ( !div.addEventListener && div.attachEvent && div.fireEvent ) {
-		div.attachEvent( "onclick", clickFn = function() {
-			// Cloning a node shouldn't copy over any
-			// bound event handlers (IE does this)
-			support.noCloneEvent = false;
-		});
-		div.cloneNode( true ).fireEvent("onclick");
-		div.detachEvent( "onclick", clickFn );
-	}
-
-	// Check if a radio maintains its value
-	// after being appended to the DOM
-	input = document.createElement("input");
-	input.value = "t";
-	input.setAttribute( "type", "radio" );
-	support.radioValue = input.value === "t";
-
-	input.setAttribute( "checked", "checked" );
-
-	// #11217 - WebKit loses check when the name is after the checked attribute
-	input.setAttribute( "name", "t" );
-
-	div.appendChild( input );
-	fragment = document.createDocumentFragment();
-	fragment.appendChild( div.lastChild );
-
-	// WebKit doesn't clone checked state correctly in fragments
-	support.checkClone = fragment.cloneNode( true ).cloneNode( true ).lastChild.checked;
-
-	// Check if a disconnected checkbox will retain its checked
-	// value of true after appended to the DOM (IE6/7)
-	support.appendChecked = input.checked;
-
-	fragment.removeChild( input );
-	fragment.appendChild( div );
-
-	// Technique from Juriy Zaytsev
-	// http://perfectionkills.com/detecting-event-support-without-browser-sniffing/
-	// We only care about the case where non-standard event systems
-	// are used, namely in IE. Short-circuiting here helps us to
-	// avoid an eval call (in setAttribute) which can cause CSP
-	// to go haywire. See: https://developer.mozilla.org/en/Security/CSP
-	if ( !div.addEventListener ) {
-		for ( i in {
-			submit: true,
-			change: true,
-			focusin: true
-		}) {
-			eventName = "on" + i;
-			isSupported = ( eventName in div );
-			if ( !isSupported ) {
-				div.setAttribute( eventName, "return;" );
-				isSupported = ( typeof div[ eventName ] === "function" );
-			}
-			support[ i + "Bubbles" ] = isSupported;
-		}
-	}
-
-	// Run tests that need a body at doc ready
-	domready(function() {
-		var container, div, tds, marginDiv,
-			divReset = "padding:0;margin:0;border:0;display:block;overflow:hidden;box-sizing:content-box;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;",
-			body = document.getElementsByTagName("body")[0];
-
-		if ( !body ) {
-			// Return for frameset docs that don't have a body
-			return;
-		}
-
-		container = document.createElement("div");
-		container.style.cssText = "visibility:hidden;border:0;width:0;height:0;position:static;top:0;margin-top:1px";
-		body.insertBefore( container, body.firstChild );
-
-		// Construct the test element
-		div = document.createElement("div");
-		container.appendChild( div );
-
-    //Check if table cells still have offsetWidth/Height when they are set
-    //to display:none and there are still other visible table cells in a
-    //table row; if so, offsetWidth/Height are not reliable for use when
-    //determining if an element has been hidden directly using
-    //display:none (it is still safe to use offsets if a parent element is
-    //hidden; don safety goggles and see bug #4512 for more information).
-    //(only IE 8 fails this test)
-		div.innerHTML = "<table><tr><td></td><td>t</td></tr></table>";
-		tds = div.getElementsByTagName("td");
-		tds[ 0 ].style.cssText = "padding:0;margin:0;border:0;display:none";
-		isSupported = ( tds[ 0 ].offsetHeight === 0 );
-
-		tds[ 0 ].style.display = "";
-		tds[ 1 ].style.display = "none";
-
-		// Check if empty table cells still have offsetWidth/Height
-		// (IE <= 8 fail this test)
-		support.reliableHiddenOffsets = isSupported && ( tds[ 0 ].offsetHeight === 0 );
-
-		// Check box-sizing and margin behavior
-		div.innerHTML = "";
-		div.style.cssText = "box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;padding:1px;border:1px;display:block;width:4px;margin-top:1%;position:absolute;top:1%;";
-		support.boxSizing = ( div.offsetWidth === 4 );
-		support.doesNotIncludeMarginInBodyOffset = ( body.offsetTop !== 1 );
-
-		// NOTE: To any future maintainer, we've window.getComputedStyle
-		// because jsdom on node.js will break without it.
-		if ( window.getComputedStyle ) {
-			support.pixelPosition = ( window.getComputedStyle( div, null ) || {} ).top !== "1%";
-			support.boxSizingReliable = ( window.getComputedStyle( div, null ) || { width: "4px" } ).width === "4px";
-
-			// Check if div with explicit width and no margin-right incorrectly
-			// gets computed margin-right based on width of container. For more
-			// info see bug #3333
-			// Fails in WebKit before Feb 2011 nightlies
-			// WebKit Bug 13343 - getComputedStyle returns wrong value for margin-right
-			marginDiv = document.createElement("div");
-			marginDiv.style.cssText = div.style.cssText = divReset;
-			marginDiv.style.marginRight = marginDiv.style.width = "0";
-			div.style.width = "1px";
-			div.appendChild( marginDiv );
-			support.reliableMarginRight =
-				!parseFloat( ( window.getComputedStyle( marginDiv, null ) || {} ).marginRight );
-		}
-
-		if ( typeof div.style.zoom !== "undefined" ) {
-			// Check if natively block-level elements act like inline-block
-			// elements when setting their display to 'inline' and giving
-			// them layout
-			// (IE < 8 does this)
-			div.innerHTML = "";
-			div.style.cssText = divReset + "width:1px;padding:1px;display:inline;zoom:1";
-			support.inlineBlockNeedsLayout = ( div.offsetWidth === 3 );
-
-			// Check if elements with layout shrink-wrap their children
-			// (IE 6 does this)
-			div.style.display = "block";
-			div.style.overflow = "visible";
-			div.innerHTML = "<div></div>";
-			div.firstChild.style.width = "5px";
-			support.shrinkWrapBlocks = ( div.offsetWidth !== 3 );
-
-			container.style.zoom = 1;
-		}
-
-		// Null elements to avoid leaks in IE
-		body.removeChild( container );
-		container = div = tds = marginDiv = null;
-	});
-
-	// Null elements to avoid leaks in IE
-	fragment.removeChild( div );
-	all = a = select = opt = input = fragment = div = null;
-
-	return support;
-})();
-
-}, {"domready":22}],
-22: [function(require, module, exports) {
-/*!
-  * domready (c) Dustin Diaz 2014 - License MIT
-  */
-!function (name, definition) {
-
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
-
-}('domready', function () {
-
-  var fns = [], listener
-    , doc = document
-    , hack = doc.documentElement.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
-
-
-  if (!loaded)
-  doc.addEventListener(domContentLoaded, listener = function () {
-    doc.removeEventListener(domContentLoaded, listener)
-    loaded = 1
-    while (listener = fns.shift()) listener()
-  })
-
-  return function (fn) {
-    loaded ? setTimeout(fn, 0) : fns.push(fn)
-  }
-
-});
-
-}, {}],
-20: [function(require, module, exports) {
-
-/**
- * Module exports.
- */
-
-module.exports = getDocument;
-
-// defined by w3c
-var DOCUMENT_NODE = 9;
-
-/**
- * Returns `true` if `w` is a Document object, or `false` otherwise.
- *
- * @param {?} d - Document object, maybe
- * @return {Boolean}
- * @private
- */
-
-function isDocument (d) {
-  return d && d.nodeType === DOCUMENT_NODE;
-}
-
-/**
- * Returns the `document` object associated with the given `node`, which may be
- * a DOM element, the Window object, a Selection, a Range. Basically any DOM
- * object that references the Document in some way, this function will find it.
- *
- * @param {Mixed} node - DOM node, selection, or range in which to find the `document` object
- * @return {Document} the `document` object associated with `node`
- * @public
- */
-
-function getDocument(node) {
-  if (isDocument(node)) {
-    return node;
-
-  } else if (isDocument(node.ownerDocument)) {
-    return node.ownerDocument;
-
-  } else if (isDocument(node.document)) {
-    return node.document;
-
-  } else if (node.parentNode) {
-    return getDocument(node.parentNode);
-
-  // Range support
-  } else if (node.commonAncestorContainer) {
-    return getDocument(node.commonAncestorContainer);
-
-  } else if (node.startContainer) {
-    return getDocument(node.startContainer);
-
-  // Selection support
-  } else if (node.anchorNode) {
-    return getDocument(node.anchorNode);
-  }
-}
-
-}, {}],
-21: [function(require, module, exports) {
-
-/**
- * Check if the DOM element `child` is within the given `parent` DOM element.
- *
- * @param {DOMElement|Range} child - the DOM element or Range to check if it's within `parent`
- * @param {DOMElement} parent  - the parent node that `child` could be inside of
- * @return {Boolean} True if `child` is within `parent`. False otherwise.
- * @public
- */
-
-module.exports = function within (child, parent) {
-  // don't throw if `child` is null
-  if (!child) return false;
-
-  // Range support
-  if (child.commonAncestorContainer) child = child.commonAncestorContainer;
-  else if (child.endContainer) child = child.endContainer;
-
-  // traverse up the `parentNode` properties until `parent` is found
-  var node = child;
-  while (node = node.parentNode) {
-    if (node == parent) return true;
-  }
-
-  return false;
-};
-
-}, {}],
-12: [function(require, module, exports) {
 
 exports.basename = function(path){
   return path.split('/').pop();
@@ -37315,7 +36844,7 @@ exports.extname = function(path){
   return '.' + ext;
 };
 }, {}],
-13: [function(require, module, exports) {
+12: [function(require, module, exports) {
 
 /**
  * Add CanvasRenderer stuff to the given `THREE` instance.
